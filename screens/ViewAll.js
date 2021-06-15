4b1449cf36ab247446d0f07b3381347f08c04129
Nutzs <s@lasd.com>
2021-06-15 13:16:29 +0800

    IChanges jpg name and merging

--- screens/ViewAll.js
+++ screens/ViewAll.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import SkeletonContent from 'react-native-skeleton-content';
-import React, { useState, useEffect , Component} from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
-import Constants from 'expo-constants';
+import SkeletonContent from "react-native-skeleton-content";
+import React, { useState, useEffect, Component } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import Constants from "expo-constants";
 // import { Card } from 'react-native-paper';
 
 const firstLayout = [
@@ -12,24 +12,24 @@ const firstLayout = [
     borderRadius: 30,
   },
   {
-    flexDirection: 'column',
+    flexDirection: "column",
     marginRight: 10,
     children: [
       {
         width: "100%",
         height: "50%",
-        marginBottom: 10
+        marginBottom: 10,
       },
       {
         width: "50%",
         height: "20%",
-        marginBottom: 10
+        marginBottom: 10,
       },
       {
         width: 100,
-        height: 20
-      }
-    ]
+        height: 20,
+      },
+    ],
   },
 ];
 const secondLayout = [
@@ -41,13 +41,13 @@ const secondLayout = [
   {
     width: "100%",
     height: 60,
-  }
+  },
 ];
 const thirdLayout = [
   {
     width: 220,
     height: 20,
-    marginBottom: 8
+    marginBottom: 8,
   },
   {
     width: 180,
@@ -57,94 +57,80 @@ const thirdLayout = [
 
 const INTERVAL_REFRESH = 3000;
 
+export default function () {
+  const [isLoading, setIsLoading] = useState(true);
 
-export default function() {
-    const [isLoading, setIsLoading] = useState(true);
-  
-    useEffect(() => {
-        if(!isLoading){
-           const timeoutId = setTimeout(() => setIsLoading(true), INTERVAL_REFRESH);
-           return () => clearTimeout(timeoutId);
-        }
-        else{
-          const timeoutId = setTimeout(() => setIsLoading(false), INTERVAL_REFRESH);
-          return () => clearTimeout(timeoutId);
-        }
-    }, [isLoading]);
-  
-      return (
-        <View style={styles.container}>
-          {/* <Card style={styles.card}> */}
-            <SkeletonContent
-              containerStyle={styles.top}
-              layout={firstLayout}
-              isLoading={isLoading}>
-              <View style = {imageContainerStyle.imageContainer}>
-                <View style = {imageContainerStyle.rowContainer}>
-                  {/* <View style={styles.imageContainer}> */}
-                    <Image
-                      source={require('./193756610_10225806831827515_3314437409129576622_n.jpg')}
-                      style={styles.image}
-                    />
-                  {/* </View> */}
-                  {/* <View style={styles.imageContainer}> */}
-                    <Image
-                      source={require('./193756610_10225806831827515_3314437409129576622_n.jpg')}
-                      style={styles.image}
-                    />
-                  {/* </View> */}
-                    <Image
-                        source={require('./193756610_10225806831827515_3314437409129576622_n.jpg')}
-                        style={styles.image}
-                      />
-                    <Image
-                      source={require('./193756610_10225806831827515_3314437409129576622_n.jpg')}
-                      style={styles.image}
-                    />
-                </View>
-              </View>
-              {/* <View style={styles.nested}>
+  useEffect(() => {
+    if (!isLoading) {
+      const timeoutId = setTimeout(() => setIsLoading(true), INTERVAL_REFRESH);
+      return () => clearTimeout(timeoutId);
+    } else {
+      const timeoutId = setTimeout(() => setIsLoading(false), INTERVAL_REFRESH);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [isLoading]);
+
+  return (
+    <View style={styles.container}>
+      {/* <Card style={styles.card}> */}
+      <SkeletonContent
+        containerStyle={styles.top}
+        layout={firstLayout}
+        isLoading={isLoading}
+      >
+        <View style={imageContainerStyle.imageContainer}>
+          <View style={imageContainerStyle.rowContainer}>
+            {/* <View style={styles.imageContainer}> */}
+            <Image source={require("./sample_pic.jpg")} style={styles.image} />
+            {/* </View> */}
+            {/* <View style={styles.imageContainer}> */}
+            <Image source={require("./sample_pic.jpg")} style={styles.image} />
+            {/* </View> */}
+            <Image source={require("./sample_pic.jpg")} style={styles.image} />
+            <Image source={require("./sample_pic.jpg")} style={styles.image} />
+          </View>
+        </View>
+        {/* <View style={styles.nested}>
                   <Text style={styles.normalText}>Nested 1</Text>
                   <Text style={styles.normalText}>Nested 2</Text>
                   <Text style={styles.normalText}>Nested 3</Text>
               </View> */}
-            </SkeletonContent>
-  
-            <SkeletonContent
-              containerStyle={styles.titleContainer}
-              layout={secondLayout}
-              isLoading={isLoading}>
-              <Text style={styles.bigText}>big words</Text>
-              <Text style={[styles.normalText, { marginTop: 20}]}>
-                words below big words
-              </Text>
-            </SkeletonContent>
-  
-            <SkeletonContent
-              layout={thirdLayout}
-              containerStyle={styles.descContainer}
-              isLoading={isLoading}>
-              <Text style={styles.normalText}>
-                some other words here
-              </Text>
-            </SkeletonContent>
-          {/* </Card> */}
-        </View>
-      );
-    }
+      </SkeletonContent>
 
+      <SkeletonContent
+        containerStyle={styles.titleContainer}
+        layout={secondLayout}
+        isLoading={isLoading}
+      >
+        <Text style={styles.bigText}>big words</Text>
+        <Text style={[styles.normalText, { marginTop: 20 }]}>
+          words below big words
+        </Text>
+      </SkeletonContent>
+
+      <SkeletonContent
+        layout={thirdLayout}
+        containerStyle={styles.descContainer}
+        isLoading={isLoading}
+      >
+        <Text style={styles.normalText}>some other words here</Text>
+      </SkeletonContent>
+      {/* </Card> */}
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
     padding: 8,
   },
   image: {
-    resizeMode: 'contain', // i honestly dk what this does
+    resizeMode: "contain", // i honestly dk what this does
     width: 50,
     height: 90,
     padding: 60, //why is this resizing the images??? NANI???????!?!?!?!!
@@ -152,7 +138,7 @@ const styles = StyleSheet.create({
   titleContainer: {
     width: 300,
     padding: 20,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
     flex: 2,
   },
   descContainer: {
@@ -163,16 +149,16 @@ const styles = StyleSheet.create({
   top: {
     width: 300,
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     padding: 20,
   },
   normalText: {
     fontSize: 18,
   },
   bigText: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 28,
   },
   // card: {
@@ -192,12 +178,11 @@ const imageContainerStyle = StyleSheet.create({
     width: 60,
     height: 60,
     borderRadius: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   rowContainer: {
     flexDirection: "row",
     // flex: 1,
   },
-})
-
+});
