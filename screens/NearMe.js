0a72d102bfa25f6d242d322a2443e32b5ff227f6
Nutzs <s@lasd.com>
2021-06-15 12:25:04 +0800

    Added MapView

--- screens/NearMe.js
+++ screens/NearMe.js
@@ -1,11 +1,55 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function NearMe() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  console.log(text);
+
   return (
     <View style={styles.container}>
-      <Text>NearMe!</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 1.29027,
+          longitude: 103.851959,
+          latitudeDelta: 0,
+          longitudeDelta: 0,
+        }}
+        customMapStyle={mapStyle}
+        style={styles.map}
+      >
+        {/*      <Marker
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+        /> */}
+      </MapView>
     </View>
   );
 }
@@ -17,4 +61,28 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });
+
+const mapStyle = [
+  {
+    featureType: "poi.business",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+];
